5078005a9b368071c6c8f8b8b1be30fe75bc31d2
Toprak Ali Sanli <123684477+toprak081@users.noreply.github.com>
2024-01-30 12:29:02 +0300

    wordcounts program
    isimlendirilmelerde ingilizce kullanildi

---
+++ wordcounts/wordcounts/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordcounts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metini giriniz:");
+            string text = Console.ReadLine();
+
+            int wordcount = countwordnumber(text);
+
+            Console.WriteLine($"Metinde geçen kelimelerin sayısı: {wordcount}");
+
+            Console.WriteLine("Program sona erdi. Çıkış yapmak için bir tuşa basın.");
+            Console.ReadKey();
+        }
+
+        static int countwordnumber(string text)
+        {
+            string[] words = text.Split(' ', '.', ',', '!', '?');
+            return words.Length;
+        }
+    }
+}
+
+
