95fe9d5d6c78cff7d76d1f7faa8656793f5e35b8
Toprak Ali Sanli <123684477+toprak081@users.noreply.github.com>
2024-02-04 12:12:04 +0300

    sesli harfler
    kullanilan sesli harfleri yazar

---
+++ vowels/vowels/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metini giriniz:");
+            string text = Console.ReadLine();
+            int totalvowels = countvowels(text);
+            Console.WriteLine("Toplam sesli harf sayısı: " + totalvowels);
+        }
+
+        static int countvowels(string text)
+        {
+            string vowels = "aeıioöuüAEIİOÖUÜ";
+            int totalvowels = 0;
+
+            foreach (char character in text)
+            {
+                if (vowels.Contains(character))
+                {
+                    totalvowels++;
+                }
+            }
+
+            return totalvowels;
+        }
+    }
+}
