6c57dd2d43ad98b156295155ee3b892b1417cc24
Toprak Ali Sanli <123684477+toprak081@users.noreply.github.com>
2024-02-25 23:29:20 +0300

    Findin a Palidrome program
    Kelimenin palidorm olup olmadigini kontrol ediyor

---
+++ Finding a Palindrome/Finding a Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Finding_a_Palindrome
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Bir kelime veya cümle girin: ");
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input);
+            bool isPalindrome = input.Equals(reversed);
+            if (isPalindrome)
+                Console.WriteLine("Girdi bir palindromdur.");
+            else
+                Console.WriteLine("Girdi bir palindrom değildir.");
+            Console.WriteLine("Devam etmek için bir tuşa basın...");
+            Console.ReadKey();
+        }
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
